098797c55f98fe311d552d129303a80dc67f0794
miguel <roberthzambrano235@gmail.com>
2024-12-06 21:12:48 -0500

    Se agregan lo más basico de POO

---
+++ 00 Clases y objetos/Clases y objetos/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_y_objetos
+{
+    //Clase Program 
+    internal class Program
+    {
+        //Metodo 
+        static void Main(string[] args)
+        {
+            //Para poder usar la clase debemos usar un objeto de la clase a usar 
+            //Es como hacer int number = 1;
+            Persona persona = new Persona("Miguel", 18);
+            //Luego de esto ya podemos usar la clase y sus metodos 
+            persona.SayHello();
+            Console.WriteLine("¿Deseas conocer mi nombre y mi edad?");
+            var answer = Console.ReadLine();
+            if (answer == "Yes" || answer == "yes") Console.WriteLine(persona.GetInfo());
+            else Console.WriteLine("It's ok");
+
+            //Creamos otro objeto, podemos crear tantos objetos como queramos 
+            Persona persona1 = new Persona("David", 18);
+            Console.WriteLine("Tengo además un amigo que tiene los siguientes datos: \n" + persona1.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+    /*
+    POO es el más utilizado ya que es tener clases y objetos
+    Un objeto es una representación de una entidad que tiene propiedades y tiene funcionalidades
+    Es decir un carro tiene propiedades tiene un color, tipo de motor 
+    y tiene funcionalidades como prender, frenar, correr, etc. Un objeto es eso. 
+    Una clase es el molde que nos sirve para crear estos objetos, la clase es la definición para crear 
+    estos objetos es muy parecido al tipo de dato que usamos como int, string, bool pero la diferencia es 
+    que nosotros especificar el tipo y somos libres de crear nuestro propios tipos en la clase
+    */
+
+    //Clase Persona 
+    public class Persona
+    {
+        string name;
+        int ago;
+        //Creación de un constructor 
+        //Un constructor se distingue porque se llama igual que la clase 
+        public Persona(string name, int ago)
+        {
+            //Para realizar la asignación de las variables hacemos lo siguiente
+            this.name = name; //El ultimo name es la que la funcion recibe como parametro 
+            this.ago = ago;
+
+        }
+
+        //Metodos son lo que puede hacer la clase persona 
+        public void SayHello()
+        {
+            //Este metodo es publico (public) para que asi puede ser usado fuera de la clase 
+            //si fuera privada (private) no puede usarse fuera de la clase solo dentro de esta. 
+            Console.WriteLine("Hola soy una persona de alguna parte del mundo");
+        }
+
+        public string GetInfo()
+        {
+            return name + " " + ago;
+        }
+    }
+}
---
+++ 01 Propiedades/Propiedades/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propiedades
+{
+    //Clase program
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Venta miVenta = new Venta(502, DateTime.Now);
+            miVenta.Total = -8858;
+            Console.WriteLine("El total de la venta es de: " + miVenta.Total);
+            Console.ReadKey();
+        }
+    }
+
+    public class Venta
+    {
+        //Atributos 
+        private int total;
+        DateTime date; 
+
+        //Propiedad o metodo 
+        public int Total
+        {
+            get
+            {
+                //return total.ToString("#.00");
+                return total;
+            }
+            set
+            {
+                if (value < 0) value = 0;
+                total = value;
+            }
+        }
+        
+        public string Date
+        {
+            get
+            {
+                return date.ToShortDateString();
+            }
+        }
+
+        //Constructor 
+        public Venta(int total, DateTime date)
+        {
+            this.total = total;
+            this.date = date;
+            
+        }
+    }
+}
